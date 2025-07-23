f283568b126c17e7a4e86ed6c1237108b28d4915
AlirezaBHD <alirezabhd2@gmail.com>
2025-07-23 14:57:23 +0330

    Implements AppDbContext with database configuration and entity sets.

--- Nakisa.Persistence/AppDbContext.cs
+++ Nakisa.Persistence/AppDbContext.cs
@@ -1,6 +1,50 @@
-﻿namespace Nakisa.Persistence;
+﻿using Microsoft.EntityFrameworkCore;
+using Nakisa.Domain.Entities;
 
-public class AppDbContext
+namespace Nakisa.Persistence;
+
+public class AppDbContext: DbContext
 {
-    
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        var entries = ChangeTracker
+            .Entries<BaseEntity>()
+            .Where(e => e.State == EntityState.Modified);
+
+        foreach (var entry in entries)
+        {
+            entry.Entity.ModifiedOn = DateTime.UtcNow;
+        }
+
+        return base.SaveChangesAsync(cancellationToken);
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        
+        modelBuilder.Entity<Playlist>()
+            .HasOne(sd => sd.Category)
+            .WithMany(a => a.Playlists)
+            .HasForeignKey(sd => sd.CategoryId);
+        
+        modelBuilder.Entity<Music>()
+            .HasOne(sd => sd.Playlist)
+            .WithMany(a => a.Musics)
+            .HasForeignKey(sd => sd.PlaylistId);
+        
+        modelBuilder.Entity<Music>()
+            .HasOne(sd => sd.PostedBy)
+            .WithMany(a => a.PostedMusics)
+            .HasForeignKey(sd => sd.UserId);
+        
+    }
+    public DbSet<User> Users => Set<User>();
+    public DbSet<Category> Categories => Set<Category>();
+    public DbSet<Playlist> Playlists => Set<Playlist>();
+    public DbSet<Music> Tracks => Set<Music>();
 }
