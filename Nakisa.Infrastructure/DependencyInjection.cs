eab71989193d42cedcdc1549e5fc37c73268cfca
AlirezaBHD <alirezabhd2@gmail.com>
2025-07-25 15:37:42 +0330

    Adds dependency injection configuration for application services.

--- Nakisa.Infrastructure/DependencyInjection.cs
+++ Nakisa.Infrastructure/DependencyInjection.cs
@@ -1,6 +1,38 @@
-﻿namespace Nakisa.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
+using Nakisa.Application.Bot;
+using Nakisa.Application.Bot.Interfaces;
+using Nakisa.Application.Bot.PlaylistBrowse;
+using Nakisa.Application.Bot.Register;
+using Nakisa.Application.Bot.Register.Steps;
+using Nakisa.Application.Bot.Session;
+using Nakisa.Application.Bot.SongSubmit;
+using Nakisa.Infrastructure.Bot;
 
-public class DependencyInjection
+namespace Nakisa.Infrastructure;
+
+public static class DependencyInjection
 {
-    
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    {
+        services.AddHostedService<BotService>();
+        
+        services.AddScoped<IBotFlowDispatcher, BotFlowDispatcher>();
+        
+        services.AddScoped<IPlaylistBrowseFlowHandler, PlaylistBrowseFlowHandler>();
+        
+        services.AddScoped<ISongSubmitFlowHandler,SongSubmitFlowHandler>();
+        
+        services.AddSingleton<IUserSessionService, UserSessionService>();
+        
+        services.AddScoped<IRegisterFlowHandler, RegisterFlowHandler>();
+        
+        services.AddScoped<IRegisterStepHandler,ChannelLinkPrefixStepHandler>();
+        services.AddScoped<IRegisterStepHandler,ChannelPrefixStepHandler>();
+        services.AddScoped<IRegisterStepHandler,ChooseIdentityStepHandler>();
+        services.AddScoped<IRegisterStepHandler,ChooseLinkTypeStepHandler>();
+        services.AddScoped<IRegisterStepHandler,ChoosingNicknameStepHandler>();
+        services.AddScoped<IRegisterStepHandler,SendingChannelLinkStepHandler>();
+
+        return services;
+    }
 }
