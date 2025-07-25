6af4d00782b38fd1b0ce2c42af7f3091f22fdeb5
AlirezaBHD <alirezabhd2@gmail.com>
2025-07-25 15:35:31 +0330

    Add playlist browse flow handler with unimplemented methods.

---
+++ Nakisa.Application/Bot/PlaylistBrowse/PlaylistBrowseFlowHandler.cs
@@ -0,0 +1,19 @@
+﻿using Nakisa.Application.Bot.Interfaces;
+using Nakisa.Application.Bot.Session;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace Nakisa.Application.Bot.PlaylistBrowse;
+
+public class PlaylistBrowseFlowHandler : IPlaylistBrowseFlowHandler
+{
+    public Task StartAsync(ITelegramBotClient bot, Update update, UserSession session, CancellationToken ct)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task HandleAsync(ITelegramBotClient bot, Update update, UserSession session, CancellationToken ct)
+    {
+        throw new NotImplementedException();
+    }
+}
