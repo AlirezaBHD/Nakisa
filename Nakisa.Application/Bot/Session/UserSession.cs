7f42016279dc2cbda281c996c3975e4e5080695a
AlirezaBHD <alirezabhd2@gmail.com>
2025-07-25 15:33:23 +0330

    Add UserSession class to manage user chat flow and data.

---
+++ Nakisa.Application/Bot/Session/UserSession.cs
@@ -0,0 +1,13 @@
+﻿using Nakisa.Domain.Enums;
+
+namespace Nakisa.Application.Bot.Session;
+
+public class UserSession
+{
+    public long ChatId { get; set; }
+    public UserFlow Flow { get; set; } = UserFlow.None;
+
+    public object? FlowData { get; set; }
+
+    public DateTime LastUpdate { get; set; } = DateTime.UtcNow;
+}
