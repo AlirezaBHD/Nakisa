2c9d6a3fd93ee235951495d4b4841dcd16b5f88e
AlirezaBHD <alirezabhd2@gmail.com>
2025-07-25 15:37:00 +0330

    Adds ChooseLinkTypeStepHandler to handle choosing link type step.

---
+++ Nakisa.Application/Bot/Register/Steps/ChooseLinkTypeStepHandler.cs
@@ -0,0 +1,71 @@
+using Nakisa.Application.Bot.Extensions;
+using Nakisa.Application.Bot.Interfaces;
+using Nakisa.Application.Bot.Keyboards;
+using Nakisa.Application.DTOs;
+using Nakisa.Domain.Enums;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace Nakisa.Application.Bot.Register.Steps;
+
+public class ChooseLinkTypeStepHandler : IRegisterStepHandler
+{
+    public RegisterStep Step => RegisterStep.ChooseLinkType;
+
+    public async Task HandleAsync(Update update, RegisterDto data, ITelegramBotClient bot, CancellationToken ct)
+    {
+        var callBack = update.CallbackQuery!.Data;
+        var chatId = update.GetChatId();
+        switch (callBack)
+        {
+            case "Username":
+                string? username = update.Message?.From?.Username
+                                   ?? update.CallbackQuery?.From?.Username;
+                //Check if username is not null
+
+                if (data.CaptionIdentifier == CaptionIdentifierType.Nickname)
+                {
+                    data.CaptionIdentifier = CaptionIdentifierType.NicknameAndUsername;
+                }
+                else if (data.CaptionIdentifier == CaptionIdentifierType.TelegramName)
+                {
+                    data.CaptionIdentifier = CaptionIdentifierType.TelegramNameAndUsername;
+                }
+
+                data.Username = username; //user might change that
+                        
+                await bot.EditMessageText(
+                    chatId: chatId,
+                    messageId: update.Message!.MessageId,
+                    text: "میخوای چنلت زیر پست ها نشون داده بشه؟",
+                    replyMarkup: RegisterKeyboards.ChannelLinkPrefixButton(),
+                    cancellationToken: ct);
+                
+                break;
+
+            case "ChannelLink":
+
+                data.Step = RegisterStep.SendingChannelLink;
+
+                await bot.EditMessageText(
+                    chatId: chatId,
+                    messageId: update.CallbackQuery.Message!.MessageId,
+                    text: "لینک چنلتو بفرست",
+                    cancellationToken: ct);
+
+                break;
+
+            case "None":
+                data.Step = RegisterStep.ChannelPrefix;
+
+                await bot.EditMessageText(
+                    chatId: chatId,
+                    messageId: update.CallbackQuery.Message!.MessageId,
+                    text: "میخوای چنلت زیر پست ها نشون داده بشه؟",
+                    replyMarkup: RegisterKeyboards.ChannelLinkPrefixButton(),
+                    cancellationToken: ct);
+
+                break;
+        }
+    }
+}
