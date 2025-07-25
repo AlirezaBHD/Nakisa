7719a7cd693e32861c7c415aac3f2c7de9cc79f3
AlirezaBHD <alirezabhd2@gmail.com>
2025-07-25 15:00:05 +0330

    Add RegisterDto to represent registration request data.

---
+++ Nakisa.Application/DTOs/RegisterDto.cs
@@ -0,0 +1,18 @@
+﻿using Nakisa.Domain.Enums;
+
+namespace Nakisa.Application.DTOs;
+
+public class RegisterDto
+{
+    public long ChatId { get; set; }
+    public string? TelegramId { get; set; }
+    public string? Username { get; set; }
+    public string? Nickname { get; set; }
+    public string? PersonChannelLink { get; set; }
+    public long Score { get; set; }
+    public string? Bio { get; set; }
+    public string? AvatarPath { get; set; }
+    public CaptionIdentifierType CaptionIdentifier { get; set; }
+    public ChannelIncludingType ChannelIncluding { get; set; }
+    public RegisterStep Step { get; set; }
+}
