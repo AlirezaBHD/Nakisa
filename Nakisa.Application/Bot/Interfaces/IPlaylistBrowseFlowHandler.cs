de0331562db9218e67ac9dbcc37ed6c61d71dda4
AlirezaBHD <alirezabhd2@gmail.com>
2025-07-25 15:35:03 +0330

    Add interface for handling playlist browsing flow in the bot.

---
+++ Nakisa.Application/Bot/Interfaces/IPlaylistBrowseFlowHandler.cs
@@ -0,0 +1,11 @@
+﻿using Nakisa.Application.Bot.Session;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace Nakisa.Application.Bot.Interfaces;
+
+public interface IPlaylistBrowseFlowHandler
+{
+    Task StartAsync(ITelegramBotClient bot, Update update, UserSession session, CancellationToken ct);
+    Task HandleAsync(ITelegramBotClient bot, Update update, UserSession session, CancellationToken ct);
+}
