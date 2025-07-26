91735f33f149c39f4ab76c549c303f8c75509871
AlirezaBHD <alirezabhd2@gmail.com>
2025-07-26 12:04:53 +0330

    Adds method to retrieve user ID from various update types.

--- Nakisa.Application/Bot/Extensions/UpdateExtensions.cs
+++ Nakisa.Application/Bot/Extensions/UpdateExtensions.cs
@@ -1,4 +1,5 @@
 using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
 
 namespace Nakisa.Application.Bot.Extensions;
 
@@ -17,5 +18,20 @@ public static class UpdateExtensions
     {
         return update.Message?.From?.Username
                ?? update.CallbackQuery?.From?.Username;
+    }
+    
+    public static long? GetUserId(this Update update)
+    {
+        return update.Type switch
+        {
+            UpdateType.Message => update.Message?.From?.Id,
+            UpdateType.CallbackQuery => update.CallbackQuery?.From?.Id,
+            UpdateType.InlineQuery => update.InlineQuery?.From?.Id,
+            UpdateType.ChosenInlineResult => update.ChosenInlineResult?.From?.Id,
+            UpdateType.MyChatMember => update.MyChatMember?.From?.Id,
+            UpdateType.ChatMember => update.ChatMember?.From?.Id,
+            UpdateType.ChatJoinRequest => update.ChatJoinRequest?.From?.Id,
+            _ => null
+        };
     }
 }
