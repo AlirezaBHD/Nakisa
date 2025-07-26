ebe1d5e345cb4138162da0dc6b7c43f63409d99d
AlirezaBHD <alirezabhd2@gmail.com>
2025-07-26 12:07:03 +0330

    Implement nickname uniqueness check and error handling for registration.

--- Nakisa.Application/Bot/Register/Steps/ChoosingNicknameStepHandler.cs
+++ Nakisa.Application/Bot/Register/Steps/ChoosingNicknameStepHandler.cs
@@ -2,6 +2,7 @@ using Nakisa.Application.Bot.Extensions;
 using Nakisa.Application.Bot.Interfaces;
 using Nakisa.Application.Bot.Keyboards;
 using Nakisa.Application.DTOs;
+using Nakisa.Application.Interfaces;
 using Nakisa.Domain.Enums;
 using Telegram.Bot;
 using Telegram.Bot.Types;
@@ -10,24 +11,43 @@ namespace Nakisa.Application.Bot.Register.Steps;
 
 public class ChoosingNicknameStepHandler : IRegisterStepHandler
 {
+    private readonly IUserService _userService;
+
+    public ChoosingNicknameStepHandler(IUserService userService)
+    {
+        _userService = userService;
+    }
+
     public RegisterStep Step => RegisterStep.ChoosingNickname;
 
     public async Task HandleAsync(Update update, RegisterDto data, ITelegramBotClient bot, CancellationToken ct)
     {
+        var chatId = update.GetChatId();
+
         var nickname = update.Message!.Text;
 
-        //TODO: Check if it already exists in database
-        // Validate Name
+        var isTaken = await _userService.IsNicknameTaken(nickname!);
+        if (isTaken)
+        {
+            await bot.SendMessage(
+                chatId: chatId,
+                text: "این اسم توسط یکی دیگه انتخب شده. یه اسم دیگه انتخاب کن",
+                cancellationToken: ct);
+        }
 
-        data.Nickname = nickname;
+        else
+        {
+            //TODO: Validate Name
 
-        data.Step = RegisterStep.ChooseLinkType;
+            data.Nickname = nickname;
 
-        var chatId = update.GetChatId();
-        await bot.SendMessage(
-            chatId: chatId,
-            text: "می‌خوای وقتی کسی روی اسمت کلیک کرد، به جایی وصل بشه؟",
-            replyMarkup: RegisterKeyboards.LinkTypeButton(),
-            cancellationToken: ct);
+            data.Step = RegisterStep.ChooseLinkType;
+
+            await bot.SendMessage(
+                chatId: chatId,
+                text: "می‌خوای وقتی کسی روی اسمت کلیک کرد، به جایی وصل بشه؟",
+                replyMarkup: RegisterKeyboards.LinkTypeButton(),
+                cancellationToken: ct);
+        }
     }
 }
