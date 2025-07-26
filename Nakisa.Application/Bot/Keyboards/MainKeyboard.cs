48026cf02449c9293d9bf1336a22277b724aacb4
AlirezaBHD <alirezabhd2@gmail.com>
2025-07-26 16:14:11 +0330

    Add separate keyboards for new and returning users.

--- Nakisa.Application/Bot/Keyboards/MainKeyboard.cs
+++ Nakisa.Application/Bot/Keyboards/MainKeyboard.cs
@@ -4,11 +4,30 @@ namespace Nakisa.Application.Bot.Keyboards;
 
 public static class MainKeyboard
 {
-    public static InlineKeyboardMarkup MainMenuButton() => new([
+    public static InlineKeyboardMarkup NewUserMainMenuButton() => new([
         [
-            InlineKeyboardButton.WithCallbackData("ثبت نام", "Register"),
-            InlineKeyboardButton.WithCallbackData("ارسال آهنگ", "SubmitSong"),
+            InlineKeyboardButton.WithCallbackData("ثبت نام", "Register")
+        ],
+        [
+            InlineKeyboardButton.WithCallbackData("ارسال آهنگ", "SubmitSong")
+        ],
+        [
             InlineKeyboardButton.WithCallbackData("دیدن پلیلیست ها", "ViewPlaylists")
         ]
     ]);
+    
+    
+    public static InlineKeyboardMarkup OldUserMainMenuButton() => new(
+    [
+        [
+            InlineKeyboardButton.WithCallbackData("ویرایش پروفایل", "Register")
+        ],
+        [
+            InlineKeyboardButton.WithCallbackData("ارسال آهنگ", "SubmitSong")
+        ],
+        [
+            InlineKeyboardButton.WithCallbackData("دیدن پلیلیست ها", "ViewPlaylists")
+        ]
+    ]
+        );
 }
