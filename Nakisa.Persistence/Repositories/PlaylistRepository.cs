8f6943c918264fe86940c61576fc6d1e409f6799
AlirezaBHD <alirezabhd2@gmail.com>
2025-07-27 18:13:34 +0330

    Create PlaylistRepository implementing IPlaylistRepository.

---
+++ Nakisa.Persistence/Repositories/PlaylistRepository.cs
@@ -0,0 +1,17 @@
+﻿using Nakisa.Domain.Entities;
+using Nakisa.Domain.Interfaces;
+
+namespace Nakisa.Persistence.Repositories;
+
+public class PlaylistRepository : Repository<Playlist>, IPlaylistRepository
+{
+    #region Injection
+
+    private readonly AppDbContext _context;
+    public PlaylistRepository(AppDbContext context) : base(context)
+    {
+        _context = context;
+    }
+    
+    #endregion
+}
