bab2c6f658face5f6e3312a310f3a1be9cce8910
AlirezaBHD <alirezabhd2@gmail.com>
2025-07-27 18:14:24 +0330

    Implement playlist service and its interface in application.

---
+++ Nakisa.Application/Services/PlaylistService.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using Nakisa.Application.Interfaces;
+using Nakisa.Domain.Entities;
+using Nakisa.Domain.Interfaces;
+
+namespace Nakisa.Application.Services;
+
+public class PlaylistService : Service<Playlist>, IPlaylistService
+{
+    #region Injection
+
+    private readonly IMapper _mapper;
+
+    public PlaylistService(IMapper mapper, IPlaylistRepository repository)
+        : base(mapper, repository)
+    {
+        _mapper = mapper;
+    }
+
+    #endregion
+}
