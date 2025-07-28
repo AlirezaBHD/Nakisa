ffa7ae6a8929e4613f884358480879b9f4fc9399
AlirezaBHD <alirezabhd2@gmail.com>
2025-07-28 14:48:55 +0330

    Adds MainPagePlaylistsDto to represent main page playlist data.

---
+++ Nakisa.Application/DTOs/Playlist/MainPagePlaylistsDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Nakisa.Application.DTOs.Playlist;
+
+public class MainPagePlaylistsDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string? Emoji { get; set; }
+}
