27f45af14923b97a75034465619a788b23ea0b01
AlirezaBHD <alirezabhd2@gmail.com>
2025-07-28 14:52:51 +0330

    Adds selecting playlist step handler for music submission flow.

---
+++ Nakisa.Application/Bot/MusicSubmission/Steps/SelectingPlaylistStepHandler.cs
@@ -0,0 +1,26 @@
+﻿using Nakisa.Application.Bot.Extensions;
+using Nakisa.Application.Bot.Interfaces;
+using Nakisa.Application.Bot.Keyboards;
+using Nakisa.Application.DTOs;
+using Nakisa.Application.Interfaces;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace Nakisa.Application.Bot.MusicSubmission.Steps;
+
+public class SelectingPlaylistStepHandler : IMusicSubmissionStepHandler
+{
+    private readonly ICategoryService _categoryService;
+
+    public SelectingPlaylistStepHandler(ICategoryService categoryService)
+    {
+        _categoryService = categoryService;
+    }
+
+    public Domain.Enums.MusicSubmissionStep Step => Domain.Enums.MusicSubmissionStep.SelectingPlaylist;
+
+    public async Task HandleAsync(Update update, SongSubmissionDto data, ITelegramBotClient bot, CancellationToken ct)
+    {
+
+    }
+}
