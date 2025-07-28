bc728b9b03460a75e34997e55365f4074eef812d
AlirezaBHD <alirezabhd2@gmail.com>
2025-07-28 14:49:13 +0330

    Add automapper profile for mapping Playlist to MainPagePlaylistsDto.

---
+++ Nakisa.Application/Mapping/PlaylistProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using Nakisa.Application.DTOs.Playlist;
+using Nakisa.Domain.Entities;
+
+namespace Nakisa.Application.Mapping;
+
+public class PlaylistProfile : Profile
+{
+    public PlaylistProfile()
+    {
+        CreateMap<Playlist, MainPagePlaylistsDto>();
+    }
+}
