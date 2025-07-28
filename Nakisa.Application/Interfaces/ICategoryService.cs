0f5b026035a0ce9909b4f257db29d81937426a93
AlirezaBHD <alirezabhd2@gmail.com>
2025-07-28 14:46:32 +0330

    Implements category service and its interface with DI and inheritance.

---
+++ Nakisa.Application/Interfaces/ICategoryService.cs
@@ -0,0 +1,8 @@
+﻿using Nakisa.Application.DTOs.Category;
+using Nakisa.Domain.Entities;
+
+namespace Nakisa.Application.Interfaces;
+
+public interface ICategoryService : IService<Category>
+{
+}
---
+++ Nakisa.Application/Services/CategoryService.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using Nakisa.Application.DTOs.Category;
+using Nakisa.Application.Interfaces;
+using Nakisa.Domain.Entities;
+using Nakisa.Domain.Enums;
+using Nakisa.Domain.Interfaces;
+
+namespace Nakisa.Application.Services;
+
+public class CategoryService : Service<Category>, ICategoryService
+{
+    #region Injection
+
+    private readonly IMapper _mapper;
+
+    public CategoryService(IMapper mapper, ICategoryRepository repository)
+        : base(mapper, repository)
+    {
+        _mapper = mapper;
+    }
+
+    #endregion
+}
