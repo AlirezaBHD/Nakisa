dec0110088db36ba7e288410327a8ce1003afe97
AlirezaBHD <alirezabhd2@gmail.com>
2025-07-28 14:42:05 +0330

    Implements MusicService and IMusicService with basic functionality.

---
+++ Nakisa.Application/Services/MusicService.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using Nakisa.Application.Interfaces;
+using Nakisa.Domain.Entities;
+using Nakisa.Domain.Interfaces;
+
+namespace Nakisa.Application.Services;
+
+public class MusicService : Service<Music>, IMusicService
+{
+    #region Injection
+
+    private readonly IMapper _mapper;
+
+    public MusicService(IMapper mapper, IMusicRepository repository)
+        : base(mapper, repository)
+    {
+        _mapper = mapper;
+    }
+
+    #endregion
+}
