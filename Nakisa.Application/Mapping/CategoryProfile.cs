c8bce4dc6126bc500e8f0afb298d964dcec850e4
AlirezaBHD <alirezabhd2@gmail.com>
2025-07-28 14:49:42 +0330

    Add CategoryProfile to map Category entity to GetCategoryDto.

---
+++ Nakisa.Application/Mapping/CategoryProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Nakisa.Application.DTOs.Category;
+using Nakisa.Domain.Entities;
+
+namespace Nakisa.Application.Mapping;
+
+public class CategoryProfile : Profile
+{
+    public CategoryProfile()
+    {
+        CreateMap<Category, GetCategoryDto>()
+            .ForMember(dest => dest.Playlists, opt =>
+                opt.MapFrom(src => src.Playlists
+                    .Where(p => p.IsActive && p.IncludeInMainPage)));
+    }
+}
