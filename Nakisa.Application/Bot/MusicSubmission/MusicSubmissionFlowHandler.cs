881b098a397a0dee2ea7e05afef36b047a7ecba2
AlirezaBHD <alirezabhd2@gmail.com>
2025-07-28 14:51:01 +0330

    Replaces SongSubmitFlowHandler with MusicSubmissionFlowHandler implementation.

---
+++ Nakisa.Application/Bot/MusicSubmission/MusicSubmissionFlowHandler.cs
@@ -0,0 +1,66 @@
+﻿using Nakisa.Application.Bot.Extensions;
+using Nakisa.Application.Bot.Interfaces;
+using Nakisa.Application.Bot.Keyboards;
+using Nakisa.Application.Bot.Session;
+using Nakisa.Application.DTOs;
+using Nakisa.Application.Interfaces;
+using Nakisa.Domain.Enums;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace Nakisa.Application.Bot.MusicSubmission;
+
+public class MusicSubmissionFlowHandler : ISongSubmitFlowHandler
+{
+    private readonly Dictionary<Domain.Enums.MusicSubmissionStep, IMusicSubmissionStepHandler> _handlers;
+    private readonly IUserSessionService _sessionService;
+    private readonly ICategoryService _categoryService;
+
+    public MusicSubmissionFlowHandler(IUserSessionService sessionService, IEnumerable<IMusicSubmissionStepHandler> handlers, ICategoryService categoryService)
+    {
+        _sessionService = sessionService;
+        _categoryService = categoryService;
+        _handlers = handlers.ToDictionary(h => h.Step);
+    }
+
+    public async Task StartAsync(ITelegramBotClient bot, Update update, UserSession session, CancellationToken ct)
+    {
+        session.Flow = UserFlow.Registering;
+        session.FlowData = new SongSubmissionDto() { Step = Domain.Enums.MusicSubmissionStep.SelectingPlaylist };
+        _sessionService.Update(session);
+        
+        var chatId = update.GetChatId();
+        var messageId = update.GetMessageId();
+
+        var categories = await _categoryService.GetCategories();
+
+        var buttons = MusicSubmissionKeyboard.CategoriesButton(categories);
+        
+        await bot.EditMessageText(
+            chatId: chatId,
+            messageId: messageId,
+            text: "یه دسته بندی یا پلیلیست انتخاب کنید",
+            replyMarkup: buttons,
+            cancellationToken: ct);
+    }
+
+    public async Task HandleAsync(ITelegramBotClient bot, Update update, UserSession session, CancellationToken ct)
+    {
+        var data = session.FlowData as SongSubmissionDto;
+        if (data == null)
+        {
+            await bot.SendMessage(update.GetChatId(), "خطا. لطفاً /start را بزنید.", cancellationToken: ct);
+            session.Flow = UserFlow.None;
+            session.FlowData = null;
+            _sessionService.Update(session);
+            return;
+        }
+
+        if (_handlers.TryGetValue(data.Step, out var handler))
+        {
+            await handler.HandleAsync(update, data, bot, ct);
+        }
+
+        _sessionService.Update(session);
+    }
+}
