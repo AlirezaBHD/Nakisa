8e437f5e312426f6848bac03a389062956ec3443
AlirezaBHD <alirezabhd2@gmail.com>
2025-07-28 19:04:35 +0330

    Add mapping from User entity to UserDto.

--- Nakisa.Application/Mapping/UserProfile.cs
+++ Nakisa.Application/Mapping/UserProfile.cs
@@ -1,5 +1,6 @@
 using AutoMapper;
 using Nakisa.Application.DTOs;
+using Nakisa.Application.DTOs.User;
 using Nakisa.Domain.Entities;
 
 namespace Nakisa.Application.Mapping;
@@ -9,5 +10,6 @@ public class UserProfile : Profile
     public UserProfile()
     {
         CreateMap<RegisterDto, User>();
+        CreateMap<User, UserDto>();
     }
 }
