c0bcb301c91be2597fc180f767ac04cd9e3ff306
AlirezaBHD <alirezabhd2@gmail.com>
2025-07-28 17:36:42 +0330

    Add ParentPlaylistDto and PlaylistsDto data transfer objects.

---
+++ Nakisa.Application/DTOs/Playlist/ParentPlaylistDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Nakisa.Application.DTOs.Playlist;
+
+public class ParentPlaylistDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string? Emoji { get; set; }
+    public long TelegramChannelId { get; set; }
+}
---
+++ Nakisa.Application/DTOs/Playlist/PlaylistsDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Nakisa.Application.DTOs.Playlist;
+
+public class PlaylistsDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string? Emoji { get; set; }
+    public long TelegramChannelId { get; set; }
+    
+    public ParentPlaylistDto? Parent { get; set; }
+}
