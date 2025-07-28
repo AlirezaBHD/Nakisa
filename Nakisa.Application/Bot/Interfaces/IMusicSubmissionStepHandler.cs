5433364fdcf21ebf217bc3714e8cbe2dac1eab25
AlirezaBHD <alirezabhd2@gmail.com>
2025-07-28 14:52:14 +0330

    Add interface for handling music submission steps in the bot.

---
+++ Nakisa.Application/Bot/Interfaces/IMusicSubmissionStepHandler.cs
@@ -0,0 +1,13 @@
+﻿using Nakisa.Application.DTOs;
+using Nakisa.Domain.Enums;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace Nakisa.Application.Bot.Interfaces;
+
+public interface IMusicSubmissionStepHandler
+{
+    MusicSubmissionStep Step { get; }
+    Task HandleAsync(Update update, SongSubmissionDto data, ITelegramBotClient bot, CancellationToken ct);
+
+}
