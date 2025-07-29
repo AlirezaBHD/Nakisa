1eaae4e7cfd139310f2fdd4569c2acc946083a39
AlirezaBHD <alirezabhd2@gmail.com>
2025-07-29 17:15:12 +0330

    Adds BrowsePlaylistDto to represent playlist browsing information.

---
+++ Nakisa.Application/DTOs/Playlist/BrowsePlaylistDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Nakisa.Application.DTOs.Playlist;
+
+public class BrowsePlaylistDto
+{
+    public int Id { get; set; }
+    public string? Emoji { get; set; }
+    public string Name { get; set; }
+    public string ChannelInviteLink { get; set; }
+}
