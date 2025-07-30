92e3fdd678eea72d613d952ca8ecedcfc5243b95
AlirezaBHD <alirezabhd2@gmail.com>
2025-07-30 14:07:10 +0330

    Implement playlist browsing flow handler with category selection.

--- Nakisa.Application/Bot/PlaylistBrowse/PlaylistBrowseFlowHandler.cs
+++ Nakisa.Application/Bot/PlaylistBrowse/PlaylistBrowseFlowHandler.cs
@@ -7,7 +7,39 @@ namespace Nakisa.Application.Bot.PlaylistBrowse;
 
 public class PlaylistBrowseFlowHandler : IPlaylistBrowseFlowHandler
 {
-    public Task StartAsync(ITelegramBotClient bot, Update update, UserSession session, CancellationToken ct)
+    private readonly Dictionary<PlaylistBrowseStep, IPlaylistBrowseStepHandler> _handlers;
+    private readonly IUserSessionService _sessionService;
+    private readonly ICategoryService _categoryService;
+
+    public PlaylistBrowseFlowHandler(IUserSessionService sessionService, IEnumerable<IPlaylistBrowseStepHandler> handlers, ICategoryService categoryService)
+    {
+        _sessionService = sessionService;
+        _categoryService = categoryService;
+        _handlers = handlers.ToDictionary(h => h.Step);
+    }
+
+    public async Task StartAsync(ITelegramBotClient bot, Update update, UserSession session, CancellationToken ct)
+    {
+        session.Flow = UserFlow.BrowsingPlaylists;
+        session.FlowData = new PlaylistBrowseDto() { Step = PlaylistBrowseStep.BrowsePlaylist };
+        _sessionService.Update(session);
+        
+        var chatId = update.GetChatId();
+        var messageId = update.GetMessageId();
+
+        var categories = await _categoryService.GetCategories();
+
+        var buttons = MusicSubmissionKeyboard.CategoriesButton(categories);
+        
+        await bot.EditMessageText(
+            chatId: chatId,
+            messageId: messageId,
+            text: "یه دسته بندی یا پلیلیست انتخاب کنید",
+            replyMarkup: buttons,
+            cancellationToken: ct);
+    }
+
+    public async Task HandleAsync(ITelegramBotClient bot, Update update, UserSession session, CancellationToken ct)
     {
         throw new NotImplementedException();
     }
