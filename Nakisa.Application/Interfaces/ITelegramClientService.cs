072655135900a4afffcbd796094f2b0d8a181a71
AlirezaBHD <alirezabhd2@gmail.com>
2025-08-16 14:24:14 +0330

    feat: add method to create channel and group
    This commit introduces a new method
    `CreateChannelAndGroupAsync` to the
    `ITelegramClientService` interface. This method
    allows the creation of a Telegram channel and
    group, returning their IDs and invite links.

--- Nakisa.Application/Interfaces/ITelegramClientService.cs
+++ Nakisa.Application/Interfaces/ITelegramClientService.cs
@@ -3,4 +3,7 @@ namespace Nakisa.Application.Interfaces;
 public interface ITelegramClientService
 {
     Task<string?> GetChannelInfoFromLinkAsync(string link);
+
+    Task<(long channelId, long groupId, string channelInvite, string groupInvite)>
+        CreateChannelAndGroupAsync(string baseName);
 }
