e7974f8fd0f977159b9401eebea0fb4f4183421c
AlirezaBHD <alirezabhd2@gmail.com>
2025-08-17 10:58:04 +0330

    feat: add AddMusicAsync method to music service
    This commit introduces the AddMusicAsync method to the
    MusicService. This method allows adding music to a playlist
    based on audio data received from Telegram, associating it
    with a user and playlist.

--- Nakisa.Application/Services/MusicService.cs
+++ Nakisa.Application/Services/MusicService.cs
@@ -1,7 +1,9 @@
 ﻿using AutoMapper;
+using Nakisa.Application.DTOs.Music;
 using Nakisa.Application.Interfaces;
 using Nakisa.Domain.Entities;
 using Nakisa.Domain.Interfaces;
+using Telegram.Bot.Types;
 
 namespace Nakisa.Application.Services;
 
@@ -10,12 +12,32 @@ public class MusicService : Service<Music>, IMusicService
     #region Injection
 
     private readonly IMapper _mapper;
+    private readonly IUserRepository _userRepository;
 
-    public MusicService(IMapper mapper, IMusicRepository repository)
+    public MusicService(IMapper mapper, IMusicRepository repository, IUserRepository userRepository)
         : base(mapper, repository)
     {
         _mapper = mapper;
+        _userRepository = userRepository;
     }
 
     #endregion
+
+    public async Task AddMusicAsync(Audio audio, long chatId, int playlistId)
+    {
+        var userId = await _userRepository.GetUserIdByChatId(chatId);
+        var music = new AddMusicDto
+        {
+            FileId = audio.FileId,
+            Artist = audio.Performer,
+            Name = audio.Title,
+            UserId = userId,
+            PlaylistId = playlistId
+        };
+
+        var musicEntity = _mapper.Map<Music>(music);
+        
+        await Repository.AddAsync(musicEntity);
+        await Repository.SaveAsync();
+    }
 }
