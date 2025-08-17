b0c83de23b29dc6812d7a3a1e8c70d478f31e3bc
AlirezaBHD <alirezabhd2@gmail.com>
2025-08-17 10:59:18 +0330

    feat: add method to add music from telegram audio
    This commit introduces a new method, `AddMusicAsync`, to the
    `IMusicService` interface. This method allows adding music
    to the system directly from a Telegram audio object, along
    with the chat ID and playlist ID.

--- Nakisa.Application/Interfaces/IMusicService.cs
+++ Nakisa.Application/Interfaces/IMusicService.cs
@@ -1,7 +1,9 @@
 ﻿using Nakisa.Domain.Entities;
+using Telegram.Bot.Types;
 
 namespace Nakisa.Application.Interfaces;
 
 public interface IMusicService : IService<Music>
 {
+    Task AddMusicAsync(Audio audio, long chatId, int playlistId);
 }
