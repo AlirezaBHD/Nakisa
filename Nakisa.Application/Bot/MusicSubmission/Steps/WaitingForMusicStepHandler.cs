40b5deab544d6bd0722458b2cc5566a51dc26047
AlirezaBHD <alirezabhd2@gmail.com>
2025-08-17 10:59:29 +0330

    \feat: implement music submission improvements
    This commit introduces several enhancements to the music
    submission process:
    
    - Added navigation to the home page via callback query.
    - Added music service for adding music.
    - Implemented a decline button to return to the home page.
    - Refactored SendText method to handle optional inline
      keyboard markup.

--- Nakisa.Application/Bot/MusicSubmission/Steps/WaitingForMusicStepHandler.cs
+++ Nakisa.Application/Bot/MusicSubmission/Steps/WaitingForMusicStepHandler.cs
@@ -13,12 +13,17 @@ namespace Nakisa.Application.Bot.MusicSubmission.Steps;
 public class WaitingForMusicStepHandler : IMusicSubmissionStepHandler
 {
     private readonly IPlaylistService _playlistService;
+    private readonly IMusicService _musicService;
     private readonly IUserService _userService;
+    private readonly IBotNavigationService _botNavigation;
 
-    public WaitingForMusicStepHandler(IPlaylistService playlistService, IUserService userService)
+    public WaitingForMusicStepHandler(IPlaylistService playlistService, IUserService userService,
+        IMusicService musicService, IBotNavigationService botNavigation)
     {
         _playlistService = playlistService;
         _userService = userService;
+        _musicService = musicService;
+        _botNavigation = botNavigation;
     }
 
     public Domain.Enums.MusicSubmissionStep Step => Domain.Enums.MusicSubmissionStep.WaitingForMusic;
@@ -27,6 +32,11 @@ public class WaitingForMusicStepHandler : IMusicSubmissionStepHandler
     {
         var chatId = update.GetChatId();
 
+        if (update.CallbackQuery?.Data == "home page")
+        {
+            await _botNavigation.SendHomePageAsync(bot, chatId, ct);
+            return;
+        }
         if (update.Message?.Audio is not { } audio)
         {
             await SendText(bot, chatId, "یه فایل موزیک قابل قبول بفرستید", ct);
@@ -39,9 +49,17 @@ public class WaitingForMusicStepHandler : IMusicSubmissionStepHandler
 
         var playlistsInfo = await _playlistService.GetPlaylistsInfo(data.TargetPlaylistId);
 
+        await _musicService.AddMusicAsync(audio, chatId, data.TargetPlaylistId);
+
         await ProcessPlaylists(playlistsInfo);
 
-        await SendText(bot, chatId, "بازم میخوای آهنگی بفرستی به این پلیلیست؟", ct);
+        var declineButton = new InlineKeyboardMarkup([
+            [
+                InlineKeyboardButton.WithCallbackData("لغو", "home page")
+            ]
+        ]);
+
+        await SendText(bot, chatId, "بازم میخوای آهنگی بفرستی به این پلیلیست؟\n آهنگتو بفرست", ct, declineButton);
 
         async Task ProcessPlaylists(IEnumerable<PlaylistsDto> playlists)
         {
@@ -60,7 +78,7 @@ public class WaitingForMusicStepHandler : IMusicSubmissionStepHandler
                     await SendAudioToChannel(bot, playlist.TelegramChannelId, fileId, messageCaption, button, ct);
 
                 var resultMessage = FormatMessage(playlist, childMessage);
-                await SendText(bot, chatId, resultMessage, ct);
+                await SendText(bot, chatId, resultMessage, ct, button);
             }
         }
     }
@@ -75,13 +93,12 @@ public class WaitingForMusicStepHandler : IMusicSubmissionStepHandler
     }
 
     private async Task<Message> SendAudioToChannel(ITelegramBotClient bot, long channelId, string fileId,
-        string caption,InlineKeyboardMarkup button, CancellationToken ct)
+        string caption, InlineKeyboardMarkup button, CancellationToken ct)
     {
         return await bot.SendAudio(
             chatId: channelId,
             audio: fileId,
             caption: caption,
-            replyMarkup:button,
             parseMode: ParseMode.Html,
             cancellationToken: ct);
     }
@@ -94,12 +111,25 @@ public class WaitingForMusicStepHandler : IMusicSubmissionStepHandler
             $"{emoji}{info.Name}: <a href=\"{message.MessageLink()}\">لینک موزیک</a>";
     }
 
-    private async Task SendText(ITelegramBotClient bot, long chatId, string text, CancellationToken ct)
+    private async Task SendText(ITelegramBotClient bot, long chatId, string text, CancellationToken ct,
+        InlineKeyboardMarkup? button = null)
     {
-        await bot.SendMessage(
-            chatId: chatId,
-            text: text,
-            parseMode: ParseMode.Html,
-            cancellationToken: ct);
+        if (button is not null)
+        {
+            await bot.SendMessage(
+                chatId: chatId,
+                text: text,
+                replyMarkup: button,
+                parseMode: ParseMode.Html,
+                cancellationToken: ct);
+        }
+        else
+        {
+            await bot.SendMessage(
+                chatId: chatId,
+                text: text,
+                parseMode: ParseMode.Html,
+                cancellationToken: ct);
+        }
     }
 }
