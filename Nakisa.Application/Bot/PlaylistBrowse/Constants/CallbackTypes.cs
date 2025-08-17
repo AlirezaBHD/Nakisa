b7a85765d9005f73721a37b7df2a3f4f729e99f7
AlirezaBHD <alirezabhd2@gmail.com>
2025-08-17 14:54:21 +0330

    feat: add callback types for category and playlist
    This commit introduces a new file containing constants
    for callback types used in playlist browsing. These
    constants, 'Category' and 'Playlist', will improve
    code readability and maintainability.

---
+++ Nakisa.Application/Bot/PlaylistBrowse/Constants/CallbackTypes.cs
@@ -0,0 +1,7 @@
+namespace Nakisa.Application.Bot.PlaylistBrowse.Constants;
+
+public static class CallbackTypes
+{
+    public const string Category = "category";
+    public const string Playlist = "playlist";
+}
