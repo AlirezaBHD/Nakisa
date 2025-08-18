62a78ebc3ab1e67a44d8a7438c2e03ae4bb02608
AlirezaBHD <alirezabhd2@gmail.com>
2025-08-18 13:51:18 +0330

    Refactor: move and rename browse playlist step handler
    This commit moves the BrowsePlaylistStepHandler and related
    constants and utils to a new namespace to better reflect
    its role within the application's flow management.

--- Nakisa.Application/Bot/Flows/PlaylistBrowse/Steps/BrowsePlaylistStepHandler.cs
+++ Nakisa.Application/Bot/Flows/PlaylistBrowse/Steps/BrowsePlaylistStepHandler.cs
@@ -1,15 +1,15 @@
-﻿using Nakisa.Application.Bot.Extensions;
-using Nakisa.Application.Bot.Interfaces;
+﻿using Nakisa.Application.Bot.Core.Enums;
+using Nakisa.Application.Bot.Core.Extensions;
+using Nakisa.Application.Bot.Core.Interfaces;
+using Nakisa.Application.Bot.Flows.PlaylistBrowse.Utils;
 using Nakisa.Application.Bot.Keyboards;
-using Nakisa.Application.Bot.PlaylistBrowse.Utils;
 using Nakisa.Application.DTOs;
 using Nakisa.Application.Interfaces;
-using Nakisa.Domain.Enums;
 using Telegram.Bot;
 using Telegram.Bot.Types;
-using Type = Nakisa.Application.Bot.PlaylistBrowse.Constants.CallbackTypes;
+using Type = Nakisa.Application.Bot.Core.Constants.PlaylistBrowseCallbackTypes;
 
-namespace Nakisa.Application.Bot.PlaylistBrowse.Steps;
+namespace Nakisa.Application.Bot.Flows.PlaylistBrowse.Steps;
 
 public class BrowsePlaylistStepHandler : IPlaylistBrowseStepHandler
 {
