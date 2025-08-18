1c6dd7fd5b6b11fe474974b8af2334cfc42f28d5
AlirezaBHD <alirezabhd2@gmail.com>
2025-08-18 13:57:12 +0330

    feat: add music submission callback types
    This commit introduces constants for music submission
    callback types, including category, playlist, and
    playlist actions (browse, submit). These constants
    will be used to handle different types of callbacks
    related to music submissions in the bot.

---
+++ Nakisa.Application/Bot/Core/Constants/MusicSubmissionCallbackTypes.cs
@@ -0,0 +1,13 @@
+namespace Nakisa.Application.Bot.Core.Constants;
+
+public static class MusicSubmissionCallbackTypes
+{
+    public const string Category = "category";
+    public const string Playlist = "playlist";
+
+    public static class PlaylistActions
+    {
+        public const string Browse = "brows";
+        public const string Submit = "submit";
+    }
+}
