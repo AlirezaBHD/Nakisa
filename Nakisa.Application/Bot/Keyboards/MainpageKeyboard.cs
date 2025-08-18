5124a4852a06ce3ab63993233787b60a6e92f2d9
AlirezaBHD <alirezabhd2@gmail.com>
2025-08-18 13:57:27 +0330

    Refactor: rename MainKeyboard to MainpageKeyboard and update types
    This commit renames the `MainKeyboard` class to
    `MainpageKeyboard` for better clarity and consistency.
    It also updates the callback types used within the class
    to reflect the new location of the constants.

---
+++ Nakisa.Application/Bot/Keyboards/MainpageKeyboard.cs
@@ -0,0 +1,34 @@
+using Telegram.Bot.Types.ReplyMarkups;
+using Type = Nakisa.Application.Bot.Core.Constants.MainPageCallbackTypes;
+
+namespace Nakisa.Application.Bot.Keyboards;
+
+public static class MainpageKeyboard
+{
+    public static InlineKeyboardMarkup NewUserMainMenuButton() => new([
+        [
+            InlineKeyboardButton.WithCallbackData("ثبت نام", Type.Register)
+        ],
+        [
+            InlineKeyboardButton.WithCallbackData("ارسال آهنگ", Type.SubmitSong)
+        ],
+        [
+            InlineKeyboardButton.WithCallbackData("دیدن پلیلیست ها", Type.ViewPlaylists)
+        ]
+    ]);
+    
+    
+    public static InlineKeyboardMarkup OldUserMainMenuButton() => new(
+    [
+        [
+            InlineKeyboardButton.WithCallbackData("ویرایش پروفایل", Type.Register)
+        ],
+        [
+            InlineKeyboardButton.WithCallbackData("ارسال آهنگ", Type.SubmitSong)
+        ],
+        [
+            InlineKeyboardButton.WithCallbackData("دیدن پلیلیست ها", Type.ViewPlaylists)
+        ]
+    ]
+        );
+}
