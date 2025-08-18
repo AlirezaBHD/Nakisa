e9a8b7b94c5c115cc61fc9c551f976b2d87765fe
AlirezaBHD <alirezabhd2@gmail.com>
2025-08-18 13:57:20 +0330

    feat: add main page callback types constant
    This commit introduces a new constant class,
    `MainPageCallbackTypes`, to store the callback
    data types used in the main page interactions.
    This improves code readability and maintainability.

---
+++ Nakisa.Application/Bot/Core/Constants/MainPageCallbackTypes.cs
@@ -0,0 +1,8 @@
+namespace Nakisa.Application.Bot.Core.Constants;
+
+public static class MainPageCallbackTypes
+{
+    public const string Register = "register";
+    public const string SubmitSong = "submit_song";
+    public const string ViewPlaylists = "view_playlists";
+}
