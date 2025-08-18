b0a986592361d4cbaee621be4787ad4c2ba38639
AlirezaBHD <alirezabhd2@gmail.com>
2025-08-18 10:46:38 +0330

    feat: add callback types constants
    This commit introduces a new file containing constants
    for callback types used in the bot application. These
    constants define the different types of callbacks
    that the bot can handle, such as "register",
    "submit_song", and "view_playlists".

---
+++ Nakisa.Application/Bot/Constants/CallbackTypes.cs
@@ -0,0 +1,8 @@
+namespace Nakisa.Application.Bot.Constants;
+
+public static class CallbackTypes
+{
+    public const string Register = "register";
+    public const string SubmitSong = "submit_song";
+    public const string ViewPlaylists = "view_playlists";
+}
