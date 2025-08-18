b85e03222bc8872b2bcc4918941782acb7bfee45
AlirezaBHD <alirezabhd2@gmail.com>
2025-08-18 13:55:56 +0330

    feat: add register callback types constant
    This commit introduces a new constant class,
    `RegisterCallbackTypes`, to define string constants
    used for identifying different types of callbacks
    during the registration process. These constants
    include values for telegram name, nickname,
    unknown, yes, no, username, channel link, and none.

---
+++ Nakisa.Application/Bot/Core/Constants/RegisterCallbackTypes.cs
@@ -0,0 +1,13 @@
+namespace Nakisa.Application.Bot.Core.Constants;
+
+public class RegisterCallbackTypes
+{
+    public const string TelegramName = "telegram_name";
+    public const string Nickname = "nickname";
+    public const string Unknown = "unknown";
+    public const string Yes = "yes";
+    public const string No = "no";
+    public const string Username = "username";
+    public const string ChannelLink = "channel_link";
+    public const string None = "none";
+}
