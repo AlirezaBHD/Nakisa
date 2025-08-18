bf3f5c78806dbcb98a9a920985c2a7e1371e46d6
AlirezaBHD <alirezabhd2@gmail.com>
2025-08-18 13:53:56 +0330

    feat: add playlist browse callback types constant
    This commit introduces a new constant class for playlist
    browse callback types. This class defines the constants
    used for category, playlist, and back actions during
    playlist browsing.

---
+++ Nakisa.Application/Bot/Core/Constants/PlaylistBrowseCallbackTypes.cs
@@ -0,0 +1,8 @@
+namespace Nakisa.Application.Bot.Core.Constants;
+
+public static class PlaylistBrowseCallbackTypes
+{
+    public const string Category = "category";
+    public const string Playlist = "playlist";
+    public const string Back = "back";
+}
