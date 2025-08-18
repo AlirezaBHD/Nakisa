c198b6fccc41fdf947379781d9534b666dcb2c1c
AlirezaBHD <alirezabhd2@gmail.com>
2025-08-18 16:12:48 +0330

    refactor: rename MainpageKeyboard to MainPageKeyboard
    This commit renames the `MainpageKeyboard` class to
    `MainPageKeyboard` for better consistency and naming
    conventions.

--- Nakisa.Application/Bot/Keyboards/MainPageKeyboard.cs
+++ Nakisa.Application/Bot/Keyboards/MainPageKeyboard.cs
@@ -3,14 +3,11 @@ using Type = Nakisa.Application.Bot.Core.Constants.MainPageCallbackTypes;
 
 namespace Nakisa.Application.Bot.Keyboards;
 
-public static class MainpageKeyboard
+public static class MainPageKeyboard
 {
     public static InlineKeyboardMarkup NewUserMainMenuButton() => new([
         [
             InlineKeyboardButton.WithCallbackData("ثبت نام", Type.Register)
-        ],
-        [
-            InlineKeyboardButton.WithCallbackData("ارسال آهنگ", Type.SubmitSong)
         ],
         [
             InlineKeyboardButton.WithCallbackData("دیدن پلیلیست ها", Type.ViewPlaylists)
