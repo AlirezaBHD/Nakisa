e30dcbe156a564cb4a6560ec6d647b3fdfffc391
AlirezaBHD <alirezabhd2@gmail.com>
2025-08-18 13:51:41 +0330

    feat: remove unused callback types constants
    This commit removes several unused CallbackTypes.cs files
    from different bot feature folders. These files define
    constant strings for callback types that are no longer
    needed in the application. Removing them cleans up the
    codebase and reduces potential confusion.

--- Nakisa.Application/Bot/Constants/CallbackTypes.cs
+++
@@ -1,8 +0,0 @@
-namespace Nakisa.Application.Bot.Constants;
-
-public static class CallbackTypes
-{
-    public const string Register = "register";
-    public const string SubmitSong = "submit_song";
-    public const string ViewPlaylists = "view_playlists";
-}
--- Nakisa.Application/Bot/PlaylistBrowse/Constants/CallbackTypes.cs
+++
@@ -1,8 +0,0 @@
-namespace Nakisa.Application.Bot.PlaylistBrowse.Constants;
-
-public static class CallbackTypes
-{
-    public const string Category = "category";
-    public const string Playlist = "playlist";
-    public const string Back = "back";
-}
