a52c25185c263950cd797216c49ea25c3611751c
AlirezaBHD <alirezabhd2@gmail.com>
2025-08-19 22:13:39 +0330

    feat: add initial migration for database schema
    This commit introduces the initial database schema
    migration. It defines the tables for categories, users,
    playlists, and musics, along with their relationships.

---
+++ Nakisa.Persistence/Migrations/20250819184009_Initial.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Nakisa.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "categories",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    order = table.Column<int>(type: "integer", nullable: false),
+                    category_type = table.Column<int>(type: "integer", nullable: false),
+                    created_on = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    modified_on = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_categories", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    chat_id = table.Column<long>(type: "bigint", nullable: false),
+                    telegram_id = table.Column<long>(type: "bigint", nullable: true),
+                    first_name = table.Column<string>(type: "text", nullable: true),
+                    last_name = table.Column<string>(type: "text", nullable: true),
+                    username = table.Column<string>(type: "text", nullable: true),
+                    nickname = table.Column<string>(type: "text", nullable: true),
+                    person_channel_link = table.Column<string>(type: "text", nullable: true),
+                    score = table.Column<long>(type: "bigint", nullable: false),
+                    bio = table.Column<string>(type: "text", nullable: true),
+                    avatar_path = table.Column<string>(type: "text", nullable: true),
+                    caption_identifier = table.Column<int>(type: "integer", nullable: false),
+                    channel_including = table.Column<int>(type: "integer", nullable: false),
+                    is_active = table.Column<bool>(type: "boolean", nullable: false),
+                    is_staff = table.Column<bool>(type: "boolean", nullable: false),
+                    created_on = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    modified_on = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_users", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "playlists",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    emoji = table.Column<string>(type: "character varying(10)", maxLength: 10, nullable: true),
+                    category_id = table.Column<int>(type: "integer", nullable: false),
+                    telegram_channel_id = table.Column<long>(type: "bigint", nullable: false),
+                    channel_invite_link = table.Column<string>(type: "text", nullable: true),
+                    telegram_group_id = table.Column<long>(type: "bigint", nullable: false),
+                    group_invite_link = table.Column<string>(type: "text", nullable: true),
+                    parent_id = table.Column<int>(type: "integer", nullable: true),
+                    include_in_main_page = table.Column<bool>(type: "boolean", nullable: false),
+                    is_active = table.Column<bool>(type: "boolean", nullable: false),
+                    profile_image_path = table.Column<string>(type: "text", nullable: true),
+                    visibility = table.Column<int>(type: "integer", nullable: false),
+                    created_on = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    modified_on = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_playlists", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_playlists_categories_category_id",
+                        column: x => x.category_id,
+                        principalTable: "categories",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "fk_playlists_playlists_parent_id",
+                        column: x => x.parent_id,
+                        principalTable: "playlists",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "musics",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    file_id = table.Column<string>(type: "text", nullable: false),
+                    name = table.Column<string>(type: "text", nullable: true),
+                    artist = table.Column<string>(type: "text", nullable: true),
+                    user_id = table.Column<int>(type: "integer", nullable: false),
+                    playlist_id = table.Column<int>(type: "integer", nullable: false),
+                    created_on = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    modified_on = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_musics", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_musics_playlists_playlist_id",
+                        column: x => x.playlist_id,
+                        principalTable: "playlists",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "fk_musics_users_user_id",
+                        column: x => x.user_id,
+                        principalTable: "users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "ix_musics_playlist_id",
+                table: "musics",
+                column: "playlist_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_musics_user_id",
+                table: "musics",
+                column: "user_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_playlists_category_id",
+                table: "playlists",
+                column: "category_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_playlists_parent_id",
+                table: "playlists",
+                column: "parent_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "musics");
+
+            migrationBuilder.DropTable(
+                name: "playlists");
+
+            migrationBuilder.DropTable(
+                name: "users");
+
+            migrationBuilder.DropTable(
+                name: "categories");
+        }
+    }
+}
