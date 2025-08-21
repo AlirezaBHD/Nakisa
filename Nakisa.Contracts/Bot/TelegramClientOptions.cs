398c7b7176a74d469b255de6050e7097d475e790
AlirezaBHD <alirezabhd2@gmail.com>
2025-08-21 19:09:25 +0330

    feat(options): add SessionPath property to TelegramClientOptions

--- Nakisa.Contracts/Bot/TelegramClientOptions.cs
+++ Nakisa.Contracts/Bot/TelegramClientOptions.cs
@@ -5,4 +5,5 @@ public class TelegramClientOptions
     public int ApiId { get; set; }
     public string? ApiHash { get; set; }
     public string? PhoneNumber { get; set; }
+    public string? SessionPath { get; set; }
 }
