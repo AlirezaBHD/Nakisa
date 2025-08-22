88e88c752deefdadaace3f9a46eb737a278686eb
AlirezaBHD <alirezabhd2@gmail.com>
2025-08-22 21:37:42 +0330

    temp 5

--- Nakisa.Infrastructure/BotClient/TelegramClientService.cs
+++ Nakisa.Infrastructure/BotClient/TelegramClientService.cs
@@ -15,10 +15,10 @@ public class TelegramClientService : ITelegramClientService, IAsyncDisposable
     {
         _options = options.Value;
 
-        _sessionDestination = "/tmp/WTelegram.session";
-
-        if (File.Exists(_options.SessionPath) && !File.Exists(_sessionDestination))
-            File.Copy(_options.SessionPath, _sessionDestination);
+        // _sessionDestination = "/tmp/WTelegram.session";
+        //
+        // if (File.Exists(_options.SessionPath) && !File.Exists(_sessionDestination))
+        //     File.Copy(_options.SessionPath, _sessionDestination);
 
         _client = new WTelegram.Client(Config);
     }
