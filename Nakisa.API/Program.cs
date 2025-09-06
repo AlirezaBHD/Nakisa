f934a4e9202a3b711d48fd66280866954b9f0ce2
AlirezaBHD <alirezabhd2@gmail.com>
2025-09-06 18:47:56 +0330

    feat: enable swagger and static files for all environments
    This commit enables Swagger and serves static files
    regardless of the environment. This is a temporary
    solution for testing purposes. The original code, which
    only enabled these features in development, has been
    commented out.

--- Nakisa.API/Program.cs
+++ Nakisa.API/Program.cs
@@ -28,10 +28,16 @@ using (var scope = app.Services.CreateScope())
 }
 
 
-if (app.Environment.IsDevelopment())
-{
+//temp
+
+// if (app.Environment.IsDevelopment())
+// {
     app.MapOpenApi();
-}
+    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseStaticFiles();
+    app.UseSwaggerUI();
+// }
 
 app.UseHttpsRedirection();
 
