cd9f76274ead956440944f485b096aed86d9b09e
AlirezaBHD <a.bahmanzade.dev@gmail.com>
2025-09-18 13:07:31 +0330

    fix: temporary remove database migration

--- Nakisa.API/Program.cs
+++ Nakisa.API/Program.cs
@@ -34,13 +34,6 @@ services.AddInfrastructureServices(builder.Configuration);
 var app = builder.Build();
 
 
-using (var scope = app.Services.CreateScope())
-{
-    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    db.Database.Migrate();
-}
-
-
 //temp
 
 // if (app.Environment.IsDevelopment())
